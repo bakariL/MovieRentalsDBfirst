ac96d0f93f04fa9a06434c58645d69c2eaadc810
BakariL <bakarilewis@gmail.com>
2018-03-12 15:12:26 -0400

    first

---
+++ MovvvieApp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MovvvieApp.Startup))]
+namespace MovvvieApp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
